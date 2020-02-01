8a3a831154dd99e13acd1a8aec1975f9f647cb5a
Ramzi Ferchichi <ramzi.ferchichi@rubrik.com>
2020-02-01 20:46:20 +0100

    General code cleanup

--- check_rubrik_cluster_storage.py
+++ check_rubrik_cluster_storage.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python
 
-"""
-check_rubrik_cluster_storage.py
+# Copyright 2018 Rubrik, Inc.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
 
-This script gets the current used storage in the Rubrik cluster,
-and returns a warning if the usage is over 50%, and a critical
-alert if the usage is above 70%.
-
-Requires the following non-core Python modules:
-- nagiosplugin
-- rubrik_cdm
-These are both avaialble from PyPI via pip
-
-Installation:
-
-The script should be copied to the Nagios plugins directory on the machine hosting the Nagios server or the NRPE
-for example the /usr/lib/nagios/plugins folder.
-Change the execution rights of the program to allow the execution to 'all' (usually chmod 0755).
-
-Created by Tim Hynes at Rubrik
-"""
 import argparse
 import logging
+import urllib3
+
 import nagiosplugin
 import rubrik_cdm
-import urllib3
+
 
 _log = logging.getLogger('nagiosplugin')
 
-class Rubrikclusterstorage(nagiosplugin.Resource):
+
+class RubrikClusterStorage(nagiosplugin.Resource):
     """Rubrik CDM Cluster Storage
     Returns the cluster storage status Rubrik CDM cluster
     """
@@ -44,39 +47,49 @@ class Rubrikclusterstorage(nagiosplugin.Resource):
 
     def get_cluster_storage(self):
         """Gets cluster storage from Rubrik CDM"""
+
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        rk = rubrik_cdm.Connect(node_ip=self.rubrik_ip,username=self.rubrik_user,password=self.rubrik_pass)
-        rkstorage = rk.get('internal','/stats/system_storage')
-        return(rkstorage)
+
+        rk = rubrik_cdm.Connect(node_ip=self.rubrik_ip, username=self.rubrik_user, password=self.rubrik_pass)
+        storage = rk.get('internal', '/stats/system_storage')
+
+        return storage
 
     def probe(self):
-        rk_cluster_storage = self.get_cluster_storage()
-        rk_total_storage = rk_cluster_storage['total']
-        rk_available_storage = rk_cluster_storage['available']
-        rk_percent_used = 100 - int(float(rk_available_storage) / float(rk_total_storage) * 100)
-        _log.debug('Cluster storage is '+str(rk_percent_used)+'% used')
-        metric = nagiosplugin.Metric('Cluster used storage', rk_percent_used, '%', min=0, max=100, context='rk_cluster_used')
+        cluster_storage = self.get_cluster_storage()
+
+        total_storage = cluster_storage['total']
+        available_storage = cluster_storage['available']
+        
+        percent_used = 100 - int(float(available_storage) / float(total_storage) * 100)
+        _log.debug(f'Cluster storage is {percent_used}% used')
+
+        metric = nagiosplugin.Metric('Cluster used storage', percent_used, '%', min=0, max=100, context='rk_cluster_used')
+
         return metric
+
 
 def parse_args():
     argp = argparse.ArgumentParser()
+
     argp.add_argument('-s', '--rubrik_ip', help="Rubrik hostname or ip address")
     argp.add_argument('-u', '--rubrik_user', help="Rubrik username")
     argp.add_argument('-p', '--rubrik_pass', help="Rubrik password")
     argp.add_argument('-w', '--warning', metavar='RANGE', default=':50', help='return warning if occupancy is outside RANGE. Value is expressed in percentage')
     argp.add_argument('-c', '--critical', metavar='RANGE', default=':70', help='return critical if occupancy is outside RANGE. Value is expressed in percentage')
-    argp.add_argument('-v', '--verbose', action='count', default=0,
-                      help='increase output verbosity (use up to 3 times)')
-    argp.add_argument('-t', '--timeout', default=30,
-                      help='abort execution after TIMEOUT seconds')
+    argp.add_argument('-v', '--verbose', action='count', default=0, help='increase output verbosity (use up to 3 times)')
+    argp.add_argument('-t', '--timeout', default=30, help='abort execution after TIMEOUT seconds')
+
     return argp.parse_args()
+
 
 @nagiosplugin.guarded
 def main():
     args = parse_args()
-    check = nagiosplugin.Check( Rubrikclusterstorage(args.rubrik_ip, args.rubrik_user, args.rubrik_pass) )
+    check = nagiosplugin.Check(RubrikClusterStorage(args.rubrik_ip, args.rubrik_user, args.rubrik_pass))
     check.add(nagiosplugin.ScalarContext('rk_cluster_used', args.warning, args.critical))
     check.main(args.verbose, args.timeout)
+
 
 if __name__ == '__main__':
     main()
